278f0e1f83a2934e461b613b370a65bf9ac1e560
Gertjan De Mulder <gertjan.demulder@ugent.be>
2020-11-19 10:54:44 +0100

    fix #17

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 const express = require('express')
 const session = require('express-session')
 const Grant = require('grant').express()
-const grant = Grant(require('./config.json'))
 const path = require('path');
 const https = require('https')
 const pem = require('pem')
 const router = require('./routes/index')
 const fs = require('fs')
 
+
 // Configuration
-const PORT = 3000
-const USE_HTTPS = true
-const config = {
+const fpathAppConfig = path.join(__dirname, 'app-config.json')
+const appConfig = JSON.parse(fs.readFileSync(fpathAppConfig, "utf8"))
+const environment = appConfig['environment']
+// Fail if the configured environment is invalid
+if(!Object.keys(appConfig.environmentConfigs).includes(environment)){
+    let msg =`"${environment}" is an invalid environment. Valid environments are: ${Object.keys(appConfig.environmentConfigs).join(', ')}`
+    throw msg
+}
+// Initialize environment config
+const environmentConfig = appConfig.environmentConfigs[environment]
+console.log(`Environment (${environment}) configuration: `, environmentConfig)
+const PORT = environmentConfig.port
+const USE_HTTPS = environmentConfig.https
+const GRANT_CONFIG_FILENAME = path.join(__dirname, environmentConfig.grant.config_filename)
+// Initialize Grant
+const grant = Grant(require(GRANT_CONFIG_FILENAME))
+
+const logConfig = {
     logConfig : {
         logRoute : true,
         logErrors : true
     }
 }
+
 
 // Web App
 const app = express()
@@ -25,7 +41,7 @@ app.set('view engine', 'pug')
     .use(grant)
     // client-side Solid auth
     .use('/solid-auth', express.static(path.join(__dirname, 'solid-auth')))
-    .use('/', router(grant, config))
+    .use('/', router(grant, logConfig))
     .use(express.static('public'))
 
 if(USE_HTTPS){
